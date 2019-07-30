32e9c474bde4ff4288aae5bd923838b818da526a
旭 杨 <27681135@qq.com>
2019-07-31 01:15:17 +0800

    init

---
+++ pages/jinian/list/list.vue
@@ -0,0 +1,63 @@
+<template>
+	<view>
+		<view class="">
+			珍藏列表
+		</view>
+		<view class="cu-list menu-avatar">
+			
+			<view class="cu-item ">
+				<view class="cu-avatar radius lg" style="background-image:url(https://ossweb-img.qq.com/images/lol/img/champion/Morgana.png);"></view>
+				<view class="content">
+					<view class="text-pink"><view class="text-cut">莫甘娜</view></view>
+					<view class="text-gray text-sm flex"> <view class="text-cut">凯尔，你被自己的光芒变的盲目！</view></view>
+				</view>
+				<view class="action">
+					<view class="text-grey text-xs">22:20</view>
+					<view class="cu-tag round bg-red sm">5</view>
+				</view>
+			</view>
+		
+		<view class="cu-item ">
+			<view class="cu-avatar radius lg" style="background-image:url(https://ossweb-img.qq.com/images/lol/img/champion/Morgana.png);"></view>
+			<view class="content">
+				<view class="text-pink"><view class="text-cut">莫甘娜</view></view>
+				<view class="text-gray text-sm flex"> <view class="text-cut">凯尔，你被自己的光芒变的盲目！</view></view>
+			</view>
+			<view class="action">
+				<view class="text-grey text-xs">22:20</view>
+				<view class="cu-tag round bg-red sm">5</view>
+			</view>
+		</view>
+		
+		<view class="cu-item ">
+			<view class="cu-avatar radius lg" style="background-image:url(https://ossweb-img.qq.com/images/lol/img/champion/Morgana.png);"></view>
+			<view class="content">
+				<view class="text-pink"><view class="text-cut">莫甘娜</view></view>
+				<view class="text-gray text-sm flex"> <view class="text-cut">凯尔，你被自己的光芒变的盲目！</view></view>
+			</view>
+			<view class="action">
+				<view class="text-grey text-xs">22:20</view>
+				<view class="cu-tag round bg-red sm">5</view>
+			</view>
+		</view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/jinian/start/start.vue
@@ -0,0 +1,44 @@
+<template>
+	
+	<view>
+		<view class="title">
+			纪念堂
+		</view>
+		<navigator url="../../index/index" class="navtext">进入</navigator>
+		<view class="bottom">
+			死亡，不会是爱的终点，遗忘才是。
+只要记得，只要有爱，就没有离开。
+                                    —— Coco
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.title{
+	margin-top:150upx;
+	margin-bottom: 250upx;
+	font-size: 80upx;
+	text-align: center;
+	line-height: 200upx;
+}
+.navtext{
+	font-size: 40upx;
+	text-align: center;
+}
+.bottom{
+	margin: 100upx;
+}
+</style>
